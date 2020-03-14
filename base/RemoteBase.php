6053b76f9bb761497acc7c2fa7128aa456168abf
azim-ut <9420183@gmail.com>
2020-03-14 23:56:27 +0200

    refactored reauests to curl

--- base/RemoteBase.php
+++ base/RemoteBase.php
@@ -11,19 +11,40 @@ class RemoteBase{
 	    $contextData  = [
 		    "http" => [
 			    "method" => $method,
+                'timeout' => 3,
 			    "header" => 'Cookie: ' . ($_SERVER['HTTP_COOKIE']??'')."\r\n".
                             "skey:".Engine::getInstance()->prop("engine.skey")."\r\n".
                             "User-Agent:".$_SERVER['HTTP_USER_AGENT']."\r\n"
 		    ]
 	    ];
-	    $context = stream_context_create($contextData);
+
+//	    $context = stream_context_create($contextData);
         $path = $host.$path;
-	    $res = file_get_contents($path, false, $context);
+
+//	    $res = file_get_contents($path, false, $context);
+        $res = self::curlGet($path);
 	    if($trace){
 		    var_dump($path, $contextData, $res);
 	    }
 	    $obj = json_decode($res);
 
 	    return $obj->data ?? null;
+    }
+
+
+    private static function curlGet($to, $headers=array("Content-Type: application/x-www-form-urlencoded")){
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_COOKIE => "",
+            CURLOPT_URL => $to
+        ));
+        $resp = curl_exec($curl);
+        if(!$resp){
+            throw new BadResultException('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+        }
+        curl_close($curl);
+        return $resp;
     }
 }
