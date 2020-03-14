5a2ac12bafd6ca6014f4948325ea1aedc0e01ac8
azim-ut <9420183@gmail.com>
2020-03-15 00:03:55 +0200

    put cookie to request

--- base/RemoteBase.php
+++ base/RemoteBase.php
@@ -8,43 +8,45 @@ class RemoteBase{
     protected static function src($path, $method = "GET", $trace = false){
         $host = Engine::getInstance()->prop("engine.host");
 
-	    $contextData  = [
-		    "http" => [
-			    "method" => $method,
+        $contextData = [
+            "http" => [
+                "method"  => $method,
                 'timeout' => 3,
-			    "header" => 'Cookie: ' . ($_SERVER['HTTP_COOKIE']??'')."\r\n".
-                            "skey:".Engine::getInstance()->prop("engine.skey")."\r\n".
-                            "User-Agent:".$_SERVER['HTTP_USER_AGENT']."\r\n"
-		    ]
-	    ];
+                "header"  => 'Cookie: ' . ($_SERVER['HTTP_COOKIE'] ?? '') . "\r\n" .
+                             "skey:" . Engine::getInstance()->prop("engine.skey") . "\r\n" .
+                             "User-Agent:" . $_SERVER['HTTP_USER_AGENT'] . "\r\n"
+            ]
+        ];
 
 //	    $context = stream_context_create($contextData);
-        $path = $host.$path;
+        $path = $host . $path;
 
 //	    $res = file_get_contents($path, false, $context);
         $res = self::curlGet($path);
-	    if($trace){
-		    var_dump($path, $contextData, $res);
-	    }
-	    $obj = json_decode($res);
+        if($trace){
+            var_dump($path, $contextData, $res);
+        }
+        $obj = json_decode($res);
 
-	    return $obj->data ?? null;
+        return $obj->data ?? null;
     }
 
 
-    private static function curlGet($to, $headers=array("Content-Type: application/x-www-form-urlencoded")){
+    private static function curlGet($to, $headers = array("Content-Type: application/x-www-form-urlencoded")){
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_COOKIE => "",
-            CURLOPT_URL => $to
+            CURLOPT_VERBOSE        => true,
+            CURLOPT_COOKIE         => ($_SERVER['HTTP_COOKIE']??''),
+            CURLOPT_URL            => $to
         ));
         $resp = curl_exec($curl);
         if(!$resp){
             throw new BadResultException('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
         }
         curl_close($curl);
+
         return $resp;
     }
 }
