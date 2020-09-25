847638f8a716e64152eed1e9eaa002f64b369356
Aleksandr Kostiukov <akm@leongaming.com>
2020-09-25 11:52:35 +0300

    added models install

--- Engine.php
+++ Engine.php
@@ -6,175 +6,204 @@ use ContextDto;
 use core\exception\CoreErrorException;
 use core\exception\SqlException;
 use core\utils\ExceptionUtils;
+use Entity;
 use Exception;
+use ReflectionClass;
+use ReflectionException;
 
 class Engine{
-	protected static $instance = null;
-	private static $urlDirs = null;
-	private $root;
-	private $host;
-	private $locale;
-	private $settings;
+    protected static $instance = null;
+    private static $urlDirs = null;
+    private $root;
+    private $host;
+    private $locale;
+    private $settings;
 
-	public function __construct(){
-		try{
-			self::$urlDirs  = self::initDirs();
-			$this->settings = parse_ini_file(__DIR__ . '/app.ini');
-			$this->root     = $this->prop("engine.template");
-		}catch(Exception $se){
-			echo ExceptionUtils::printException($se);
-		}
-	}
+    public function __construct(){
+        try{
+            self::$urlDirs  = self::initDirs();
+            $this->settings = parse_ini_file(__DIR__ . '/app.ini');
+            $this->root     = $this->prop("engine.template");
+        }catch(Exception $se){
+            echo ExceptionUtils::printException($se);
+        }
+    }
 
-	public function sessionId(){
-		ContextDto::getInstance()->sess();
-	}
+    public function sessionId(){
+        ContextDto::getInstance()->sess();
+    }
 
-	public function builder(){
-		return $this;
-	}
+    public function builder(){
+        return $this;
+    }
 
-	public function host($host){
-		$this->host = $host;
+    public function host($host){
+        $this->host = $host;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getLocale(){
-		if($_COOKIE["locale"] ?? null){
-			return $_COOKIE["locale"];
-		}
+    public function getLocale(){
+        if($_COOKIE["locale"] ?? null){
+            return $_COOKIE["locale"];
+        }
 
-		return $this->prop("engine.locale");
-	}
+        return $this->prop("engine.locale");
+    }
 
-	public function setLocale($locale = null){
+    public function setLocale($locale = null){
 
-		$expire = 3600 * 24 * 365;
-		setcookie("locale", $locale, time() + $expire, "/");
+        $expire = 3600 * 24 * 365;
+        setcookie("locale", $locale, time() + $expire, "/");
 
-		return $locale;
-	}
+        return $locale;
+    }
 
-	public function templateRoot($root){
-		$this->root = $root;
+    public function templateRoot($root){
+        $this->root = $root;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public static function getInstance(){
-		self::$instance;
-		if(is_null(self::$instance)){
-			self::$instance = new Engine();
-		}
+    public static function getInstance(){
+        self::$instance;
+        if(is_null(self::$instance)){
+            self::$instance = new Engine();
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
 
-	public function prop($name){
-		if(isset($this->settings[ $name ])){
-			return $this->settings[ $name ];
-		}
+    public function prop($name){
+        if(isset($this->settings[ $name ])){
+            return $this->settings[ $name ];
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public static function getDir($i){
-		if(!self::$urlDirs){
-			self::$urlDirs = self::initDirs();
-		}
-		$dirs = self::$urlDirs;
-		if(sizeof($dirs) > $i){
-			return $dirs[ $i ];
-		}
+    public static function getDir($i){
+        if(!self::$urlDirs){
+            self::$urlDirs = self::initDirs();
+        }
+        $dirs = self::$urlDirs;
+        if(sizeof($dirs) > $i){
+            return $dirs[ $i ];
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public function getDirsLegacy($from = 0, $to = 0){
-		$dirs = self::$urlDirs;
-		$res  = $dirs;
-		if($to > 0){
-			$to += $from;
-		}
-		if($from > sizeof($dirs) && $to < 1){
-			return array();
-		}
-		if($from > 0 || $to > 0){
-			$res = array();
-			foreach($dirs as $i => $dir){
-				if($to > 0 && $i >= $to){
-					break;
-				}
-				if($i >= $from){
-					$res[] = $dir;
-				}
-			}
-		}
+    public function getDirsLegacy($from = 0, $to = 0){
+        $dirs = self::$urlDirs;
+        $res  = $dirs;
+        if($to > 0){
+            $to += $from;
+        }
+        if($from > sizeof($dirs) && $to < 1){
+            return array();
+        }
+        if($from > 0 || $to > 0){
+            $res = array();
+            foreach($dirs as $i => $dir){
+                if($to > 0 && $i >= $to){
+                    break;
+                }
+                if($i >= $from){
+                    $res[] = $dir;
+                }
+            }
+        }
 
-		return $res;
-	}
+        return $res;
+    }
 
-	public function getTemplateFile(){
-		$dirs = array();
-		foreach(self::$urlDirs as $dir){
-			$dirs[] = $dir;
-		}
-		$templateRoot = __DIR__ . "/../" . $this->root;
-		$target       = null;
-		while(!empty($dirs) && !$target){
-			$checkDir = implode("/", $dirs);
-			if(file_exists($templateRoot . $checkDir . ".php")){
-				$target = $checkDir . ".php";
-			}
-			if(!$target && file_exists($templateRoot . $checkDir . "/index.php")){
-				$target = $checkDir . "/index.php";
-			}
-			array_pop($dirs);
-		}
+    public function getTemplateFile(){
+        $dirs = array();
+        foreach(self::$urlDirs as $dir){
+            $dirs[] = $dir;
+        }
+        $templateRoot = __DIR__ . "/../" . $this->root;
+        $target       = null;
+        while(!empty($dirs) && !$target){
+            $checkDir = implode("/", $dirs);
+            if(file_exists($templateRoot . $checkDir . ".php")){
+                $target = $checkDir . ".php";
+            }
+            if(!$target && file_exists($templateRoot . $checkDir . "/index.php")){
+                $target = $checkDir . "/index.php";
+            }
+            if(!$target && file_exists($templateRoot . $checkDir . ".html")){
+                $target = $checkDir . ".html";
+            }
+            if(!$target && file_exists($templateRoot . $checkDir . "/index.html")){
+                $target = $checkDir . "/index.html";
+            }
+            array_pop($dirs);
+        }
 
-		if(file_exists($templateRoot . $target) && is_file($templateRoot . $target)){
-			return $templateRoot . $target;
-		}
-		return null;
-	}
+        if(file_exists($templateRoot . $target) && is_file($templateRoot . $target)){
+            return $templateRoot . $target;
+        }
 
-	//---------------------------------------
-	public function runProject(){
-		$path = $this->getTemplateFile();
-		try{
-			if(!$path){
-				http_response_code(404);
-				if(file_exists(__DIR__ . "/" . $this->root . "errors/404.php")){
-					require_once __DIR__ . "/" . $this->root . "errors/404.php";
-				}
-				exit();
-			}else{
-				require_once($path);
-			}
-		}catch(SqlException | CoreErrorException $se){
-			require_once(__DIR__ . "/../" . $this->root . "errors/500.php");
-			error_log(nl2br(ExceptionUtils::printException($se)));
-		}catch(Exception $e){
-			require_once(__DIR__ . "/../" . $this->root . "errors/500.php");
+        return null;
+    }
 
-			return false;
-			//echo $e->getFile() . " #" . $e->getLine() . "<br/>";
-		}
+    //---------------------------------------
+    public function runProject(){
+        $path = $this->getTemplateFile();
+        try{
+            if(!$path){
+                http_response_code(404);
+                if(file_exists(__DIR__ . "/" . $this->root . "errors/404.php")){
+                    require_once __DIR__ . "/" . $this->root . "errors/404.php";
+                }
+                exit();
+            }else{
+                require_once($path);
+            }
+        }catch(SqlException | CoreErrorException $se){
+            require_once(__DIR__ . "/../" . $this->root . "errors/500.php");
+            error_log(nl2br(ExceptionUtils::printException($se)));
+        }catch(Exception $e){
+            require_once(__DIR__ . "/../" . $this->root . "errors/500.php");
 
-		return true;
-	}
+            return false;
+            //echo $e->getFile() . " #" . $e->getLine() . "<br/>";
+        }
 
-	private static function initDirs(){
-		$tmp = trim($_SERVER["REQUEST_URI"], "/");
-		$tmp = preg_replace("#(\?.+$)#mi", "", $tmp);
-		$tmp = trim($tmp, "/");
-		$tmp = trim($tmp, "?");
+        return true;
+    }
 
-		return preg_split("#\/#mi", $tmp);
-	}
+    private static function initDirs(){
+        $tmp = trim($_SERVER["REQUEST_URI"], "/");
+        $tmp = preg_replace("#(\?.+$)#mi", "", $tmp);
+        $tmp = trim($tmp, "/");
+        $tmp = trim($tmp, "?");
 
-	public function getDirs($from = 0){
-		return array_slice(self::$urlDirs, $from);
-	}
+        return preg_split("#\/#mi", $tmp);
+    }
+
+    public function getDirs($from = 0){
+        return array_slice(self::$urlDirs, $from);
+    }
+
+    public static function install(){
+        $classes           = get_declared_classes();
+        $implementsIModule = array();
+        foreach($classes as $klass){
+            try{
+                $reflect = new ReflectionClass($klass);
+                if($reflect->implementsInterface(Entity::class)){
+                    $implementsIModule[] = $klass;
+                }
+            }catch(ReflectionException $e){
+
+            }
+        }
+        foreach($implementsIModule as $row){
+            $res = call_user_func(array((new $row()), "install"));
+            echo $res."\r\n";
+        }
+    }
 }
